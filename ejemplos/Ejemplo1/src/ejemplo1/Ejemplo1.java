14cea7c25a6bd6e2227d94a9a708197b7b16eb7b
CesarM3-A <cesarmateoab2007@gmail.com>
2025-11-12 11:48:35 -0500

    Promedio

--- ejemplos/Ejemplo1/src/ejemplo1/Ejemplo1.java
+++ ejemplos/Ejemplo1/src/ejemplo1/Ejemplo1.java
@@ -7,7 +7,8 @@
  * un mensaje como sigue: Estudiante aprobado con un promedio: 8.1
  */
 package ejemplo1;
-
+import java.util.Locale;
+import java.util.Scanner;
 /**
  *
  * @author reroes
@@ -16,8 +17,14 @@ public class Ejemplo1 {
 
     public static void main(String[] args) {
         // Condicionales simplesmain
+Scanner entrada = new Scanner(System.in);
+entrada.useLocale(Locale.US); 
 
-        double promedio = 7.5;
+
+        double promedio;
+        
+        System.out.println("Ingrese el valor de la nota");
+        promedio = entrada.nextDouble();
 
         if (promedio >= 7.5) {
             System.out.printf("Estudiante aprobado con un "
@@ -29,8 +36,8 @@ public class Ejemplo1 {
                     + "promedio: %.2f\n", promedio);
         }
 
-        if (promedio < 7.5) {
-            System.out.printf("Estudiante aprobado con un "
+        if (promedio > 7.5) {
+            System.out.printf("Estudiante reprobado con un "
                     + "promedio: %.2f\n", promedio);
         }
     }
